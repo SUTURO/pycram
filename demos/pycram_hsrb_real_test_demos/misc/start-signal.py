9718ebcd9fc036b56cd3d311ebe76a1759907aa2
Celina SuturoLaptop <celina5@uni-bremen.de>
2024-07-19 12:42:40 +0200

    [robocup] few changes for the robocup

--- demos/pycram_hsrb_real_test_demos/misc/start-signal.py
+++ demos/pycram_hsrb_real_test_demos/misc/start-signal.py
@@ -1,10 +1,12 @@
 import rospy
-from pycram.utilities.robocup_utils import StartSignalWaiter
+from pycram.utilities.robocup_utils import StartSignalWaiter, TextToSpeechPublisher
 
 # Create an instance of the StartSignalWaiter
 start_signal_waiter = StartSignalWaiter()
 #load everfyhing world giskard robokudo....
 # Wait for the start signal
+talk = TextToSpeechPublisher()
+talk.pub_now("heey")
 start_signal_waiter.wait_for_startsignal()
 
 # Once the start signal is received, continue with the rest of the script
