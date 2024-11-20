d63512de3e6e38d08e102de71dee5fafc9cd6d89
pkehr <philk99@web.de>
2024-11-20 12:54:52 +0100

    [joint_states] (temporary fix) added condition to skip if a given joint_name is not in robot description

--- src/pycram/ros_utils/robot_state_updater.py
+++ src/pycram/ros_utils/robot_state_updater.py
@@ -56,7 +56,8 @@ class RobotStateUpdater:
         try:
             msg = wait_for_message(self.joint_state_topic, JointState)
             for name, position in zip(msg.name, msg.position):
-                World.robot.set_joint_position(name, position)
+                if name in list(World.robot.joints.keys()):
+                    World.robot.set_joint_position(name, position)
         except AttributeError:
             pass
 
