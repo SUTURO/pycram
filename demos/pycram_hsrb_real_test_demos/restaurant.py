3f41c09c6ac5a73be1ceb495a59d3fbfce264cf6
JuliSlz <juschulz@uni-bremen.de>
2024-11-29 10:00:47 +0100

    [restaurant] updated lookAround

--- demos/pycram_hsrb_real_test_demos/restaurant.py
+++ demos/pycram_hsrb_real_test_demos/restaurant.py
@@ -6,7 +6,8 @@ import rospy
 from geometry_msgs.msg import WrenchStamped, PoseStamped, PointStamped, PoseWithCovarianceStamped
 from robokudo_msgs.msg import QueryAction, QueryGoal
 
-from pycram.demos.pycram_hsrb_real_test_demos.utils.misc_restaurant import Restaurant, monitor_func
+from demos.pycram_hsrb_real_test_demos.utils.misc_restaurant import Restaurant
+# from pycram.demos.pycram_hsrb_real_test_demos.utils.misc_restaurant import Restaurant, monitor_func
 from pycram.designators.action_designator import ParkArmsAction, DetectAction, LookAtAction, MoveTorsoAction
 from pycram.designators.motion_designator import MoveGripperMotion
 from pycram.datastructures.enums import Arms, ImageEnum
@@ -17,7 +18,7 @@ from pycram.failures import SensorMonitoringCondition, HumanNotFoundCondition
 from pycram.datastructures.pose import Pose
 from pycram.process_module import real_robot
 from demos.pycram_hsrb_real_test_demos.utils.startup import startup
-import pycram.external_interfaces.giskard as giskardpy
+# import pycram.external_interfaces.giskard as giskardpy
 import pycram.external_interfaces.robokudo as robokudo
 
 import rospy
@@ -36,26 +37,28 @@ world, v, text_to_speech_publisher, image_switch_publisher, move, robot = startu
 marker = ManualMarkerPublisher()
 listener = tf.TransformListener()
 
-
 # Initialize global variable
 global human_bool
+human_bool = False
 
-def monitor_func_human(humanPoint: PointStamped, robotPoint:PoseStamped):
+
+def monitor_func_human(humanPoint: PointStamped, robotPoint: PoseStamped):
     restaurant.set_human_pose(humanPoint)
     tmp_dist = restaurant.distance()
     if tmp_dist < 0.4:
         print("min dist reached")
         return SensorMonitoringCondition
-    if  humanPoint.point is None:
+    if humanPoint.point is None:
         print("Human went missing lol. Where did he go?")
         return False
+
 
 #
 # def monitor_func_human():
 #     der = fts.get_last_value()
 #     if abs(der.wrench.force.x) > 10.30:
 #         print("sensor")
- #      return SensorMonitoringCondition
+#      return SensorMonitoringCondition
 #     if not human.human_pose.get_value():
 #         print("human")
 #         return HumanNotFoundCondition
@@ -112,11 +115,43 @@ class Human:
             self.human_pose.set_value(False)
 
 
+class Restaurant:
+    def __init__(self):
+        self.toya_pose = Fluent()
+        self.human_pose = None
+        self.toya_pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.toya_pose_cb)
+        self.robot = robot
+        self.rospy = rospy
 
-restaurant = Restaurant(robot, rospy)
+    def toya_pose_cb(self, msg):
+        # print("updating")
+        self.toya_pose.set_value(robot.get_pose())
+        self.rospy.sleep(0.5)
+
+    def set_human_pose(self, pose: PoseWithCovarianceStamped):
+        self.human_pose = pose
+
+    def distance(self):
+        print("toya pose:" + str(self.toya_pose.get_value().pose))
+        if self.human_pose:
+            dis = math.sqrt((self.human_pose.pose.position.x - self.toya_pose.get_value().pose.position.x) ** 2 +
+                            (self.human_pose.pose.position.y - self.toya_pose.get_value().pose.position.y) ** 2)
+            print("dis: " + str(dis))
+            return dis
+        else:
+            self.rospy.logerr("Cant calculate distance, no human pose found")
 
 
-def transform_pose(input_pose, from_frame, to_frame, rospy:):
+def monitor_func(restaurant: Restaurant):
+    if restaurant.distance() < 1:
+        return SensorMonitoringCondition
+    return False
+
+
+restaurant = Restaurant()
+
+
+def transform_pose(input_pose, from_frame, to_frame):
     """
     Transforms a pose from 'from_frame' to 'to_frame'.
     """
@@ -138,86 +173,115 @@ rospy.loginfo("Waiting for action server")
 rospy.loginfo("You can start your demo now")
 tf_listener = LocalTransformer()
 
-def look_around(increase: float):
-    angle = 0
-    while angle <=1:
-        #todo: das minus y ist nur weil anessa faul ist sonst musst du da 0
-        look_pose = Pose([1, 0, 0.4], frame="hsrb/"+ RobotDescription.current_robot_description.base_link)
-        #todo: increment also needs minus x hehe
-        #todo: abbruch muss eign -> mensch gesehn sein
-        #look_pose_in_map = tf_listener.transformPose("/map", look_pose)
-        #look_point_in_map = look_pose_in_map.pose.position
-        # giskardpy.move_head_to_pose(look_point_in_map)
 
-        #LookAtAction([Pose([look_point_in_map.x+angle, look_point_in_map.y+angle, look_point_in_map.z])]).resolve().perform()
-        #rospy.sleep(1)
-        angle += increase
-        print(angle)
+def human_pose_cb(data):
+    query_result = data
+    return data
+
+def look_around(increase: int):
+    """
+    Function to make Toya look continuous from left to right. It stops if Toya perceives a human.
+    :param: increase: The increments in which Toya should look around.
+    """
+    for x in range(-10, 10, increase):
+        angle = x / 10
+        look_pose = Pose([1, 0, 0.4], frame="hsrb/" + RobotDescription.current_robot_description.base_link)
+        # todo: abbruch muss eign -> mensch gesehn sein
+        look_pose_in_map = tf_listener.transformPose("/map", look_pose)
+        look_point_in_map = look_pose_in_map.pose.position
+
+        LookAtAction(
+            [Pose([look_point_in_map.x + angle, look_point_in_map.y + angle, look_point_in_map.z])]).resolve().perform()
+        rospy.sleep(1)
+
         if human_bool:
             break
-    while angle >=0:
-        #LookAtAction([Pose([look_point_in_map.x-angle, look_point_in_map.x-angle, look_point_in_map.z])]).resolve().perform()
-        #angle -= increase
-        print(angle)
+
+    for w in range(10, -10, -increase):
+        angle = w / 10
+        look_pose = Pose([1, 0, 0.4], frame="hsrb/" + RobotDescription.current_robot_description.base_link)
+        # todo: increment also needs minus x hehe
+        # todo: abbruch muss eign -> mensch gesehn sein
+        look_pose_in_map = tf_listener.transformPose("/map", look_pose)
+        look_point_in_map = look_pose_in_map.pose.position
+
+        LookAtAction(
+            [Pose([look_point_in_map.x + angle, look_point_in_map.y + angle, look_point_in_map.z])]).resolve().perform()
+        rospy.sleep(1)
+
         if human_bool:
             break
+
 
 def demo(step):
     with real_robot:
+        rospy.Subscriber("/human_pose", PointStamped, human_pose_cb)
         talk = True
+        human_bool = False
 
         start_pose = robot.get_pose()
         if step <= 0:
-
             text_to_speech_publisher.pub_now("Starting Restaurant Demo.", talk)
             config_for_placing = {'arm_lift_joint': -1, 'arm_flex_joint': -0.16, 'arm_roll_joint': -0.0145,
                                   'wrist_flex_joint': -1.417, 'wrist_roll_joint': 0.0}
-            perceive_conf = {'arm_lift_joint': 0.20,  'wrist_flex_joint': 1.8, 'arm_roll_joint': -1, }
+            perceive_conf = {'arm_lift_joint': 0.20, 'wrist_flex_joint': 1.8, 'arm_roll_joint': -1, }
             pakerino(config=config_for_placing)
 
-            #todo: eign müsste sie schräg stehen weil sonst arm in weg und sie muss auf 0.5 hoch das ist nur zum gucken! nicht fahren
-            #MoveTorsoAction([0.1]).resolve().perform()
-            #ParkArmsAction([Arms.LEFT]).resolve().perform()
+            # todo: eign müsste sie schräg stehen weil sonst arm in weg und sie muss auf 0.5 hoch das ist nur zum gucken! nicht fahren
+            # MoveTorsoAction([0.1]).resolve().perform()
+            # ParkArmsAction([Arms.LEFT]).resolve().perform()
 
         if step <= 1:
-            # todo look from left to right
-            look_around(0.1)
+            # TODO look around muss gleichzeitig ausgeführt werden, während perception läuft
+            #look_around(1)
             image_switch_publisher.pub_now(ImageEnum.WAVING.value)
             success = False
+
             while not success:
                 try:
-                    text_to_speech_publisher.pub_now("Please wave you hand. I will come to you", talk)
+                    # text_to_speech_publisher.pub_now("Please wave you hand. I will come to you", talk)
 
-                    restaurant.human_pose = None
-                    #todo: herrasufinden ob waving human in head_rgbd_sensor_rgb_frame oder in map uebergeben wird
-                    #
-                    #TODO for testing: Usage of query_human
-                    human_pose = robokudo.query_human()
-                    #human_pose = robokudo.query_waving_human()
+                    human_pose = None
+                                #todo: herrasufinden ob waving human in head_rgbd_sensor_rgb_frame oder in map uebergeben wird
+                                 #
+                                 #TODO for testing: Usage of query_human
+                    # human_pose = robokudo.query_human()
+                    #human_pose = robokudo.query_human()
+                    # human_dict = DetectAction(technique='human').resolve().perform()
+                    # print("Help me, i want to die ", human_dict)
+                    new_human_p = DetectAction(technique='human', state="start").resolve().perform()
+
+                    new_human_p = DetectAction(technique='human', state="stop").resolve().perform()
+                    #print("Release me from my pain: ", new_human_p)
+                    #print("id humans: " + str(id_humans))
+                    #host_pose = human_dict[id_humans[0]]
                     print(human_pose)
-
+                    #
                     if human_pose is not None:
                         human_bool = True
-                    human_pose_in_map  = human_pose
+                    human_pose_in_map = human_pose
                     human_point_in_map = human_pose_in_map.pose.position
                     # transforms the human pose that is in map to a pose relative to Toya
-                    human_pose_tf_base = tf_listener.transformPose("hsrb/"+ RobotDescription.current_robot_description.base_link, human_pose)
+                    human_pose_tf_base = tf_listener.transformPose(
+                        "hsrb/" + RobotDescription.current_robot_description.base_link, human_pose)
                     human_point_tf_base = human_pose_tf_base.pose.position
-                    try:
-                        plan = Code(lambda: rospy.sleep(1)) * 99999999 >> Monitor(monitor_func)
-                        plan.perform()
-                    except SensorMonitoringCondition:
-                        text_to_speech_publisher.pub_now("Finding my way. Please wait.", talk)
-                        image_switch_publisher.pub_now(ImageEnum.HI.value)
-
-
-                    # human_poseTm = transform_pose(human_pose, "head_rgbd_sensor_rgb_frame", "map")
-                    # human_p = human_poseTm
-                    # human_p.pose.position.z = 0
-                    # human_p.pose.orientation.x = 0
-                    # human_p.pose.orientation.y = 0
-                    # human_p.pose.orientation.z = 0
-                    # #             restaurant.human_pose = None
+                except Exception as e:
+                    print(e)
+        #          try:
+        #                 plan = Code(lambda: rospy.sleep(1)) * 99999999 >> Monitor(monitor_func)
+        #                 plan.perform()
+        #             except SensorMonitoringCondition:
+        #                 text_to_speech_publisher.pub_now("Finding my way. Please wait.", talk)
+        #                 image_switch_publisher.pub_now(ImageEnum.HI.value)
+        #
+        #
+        #             # human_poseTm = transform_pose(human_pose, "head_rgbd_sensor_rgb_frame", "map")
+        # human_p = human_poseTm
+        # human_p.pose.position.z = 0
+        # human_p.pose.orientation.x = 0
+        # human_p.pose.orientation.y = 0
+        # human_p.pose.orientation.z = 0
+        # #             restaurant.human_pose = None
         #             human_pose = robokudo.query_waving_human()
         #             human_poseTm = transform_pose(human_pose, "head_rgbd_sensor_rgb_frame", "map")
         #             human_p = human_poseTm
@@ -232,49 +296,51 @@ def demo(step):
         #                 success = True
         #                 restaurant.human_pose = human_p
         #                 # print(human_p.pose.position.x)
-                except Exception as e:
-                    print("An error occurred from perception:", e)
-        # if step <= 2:
-        #     text_to_speech_publisher.pub_now("Driving", talk)
-        #     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
-        #     world.current_bullet_world.add_vis_axis(Pose.from_pose_stamped(human_p))
-        #     #print(restaurant.distance())
-        #     marker.publish(Pose.from_pose_stamped(human_p))
-        #     world.current_bullet_world.add_vis_axis(Pose.from_pose_stamped(human_p))
-        #     try:
-        #         plan = Code(lambda: move.pub_now(human_p)) >> Monitor(monitor_func)
-        #         plan.perform()
-        #     except SensorMonitoringCondition:
-        #         print("ABBRUCH")
-        #         move.interrupt()
-        #     # robot_pose_to_human = Pose.from_pose_stamped(calc_distance(human_p, 0.5))
-
-            # world.current_bullet_world.add_vis_axis(robot_pose_to_human)
-        #     move.query_pose_nav(robot_pose_to_human)
-        #     image_switch_publisher.pub_now(ImageEnum.ORDER.value)
-        #     text_to_speech_publisher.pub_now("Hi what do you want to have.", talk)
-        #     # nlp stuff
-        # if step <= 3:
-        #     text_to_speech_publisher.pub_now("Driving", talk)
-        #     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
-        #     move.query_pose_nav(start_pose)
-        #     # nlp stuff insert here
-        #     image_switch_publisher.pub_now(ImageEnum.HANDOVER.value)
-        #     text_to_speech_publisher.pub_now("The guest wants: apple, milk", talk)
-        #
-        # if step <= 4:
-        #     text_to_speech_publisher.pub_now("Driving", talk)
-        #     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
-        #     move.query_pose_nav(robot_pose_to_human)
-        #     # nlp stuff insert here
-        #     image_switch_publisher.pub_now(ImageEnum.HANDOVER.value)
-        #     text_to_speech_publisher.pub_now("Take out your order", talk)
-        #
-        # if step <= 5:
-        #     text_to_speech_publisher.pub_now("Driving", talk)
-        #     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
-        #     move.query_pose_nav(start_pose)
-        #     demo(step=1)
 
 
-demo(0)
+#                except Exception as e:
+#                   print("An error occurred from perception:", e)
+# if step <= 2:
+#     text_to_speech_publisher.pub_now("Driving", talk)
+#     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
+#     world.current_bullet_world.add_vis_axis(Pose.from_pose_stamped(human_p))
+#     #print(restaurant.distance())
+#     marker.publish(Pose.from_pose_stamped(human_p))
+#     world.current_bullet_world.add_vis_axis(Pose.from_pose_stamped(human_p))
+#     try:
+#         plan = Code(lambda: move.pub_now(human_p)) >> Monitor(monitor_func)
+#         plan.perform()
+#     except SensorMonitoringCondition:
+#         print("ABBRUCH")
+#         move.interrupt()
+#     # robot_pose_to_human = Pose.from_pose_stamped(calc_distance(human_p, 0.5))
+
+# world.current_bullet_world.add_vis_axis(robot_pose_to_human)
+#     move.query_pose_nav(robot_pose_to_human)
+#     image_switch_publisher.pub_now(ImageEnum.ORDER.value)
+#     text_to_speech_publisher.pub_now("Hi what do you want to have.", talk)
+#     # nlp stuff
+# if step <= 3:
+#     text_to_speech_publisher.pub_now("Driving", talk)
+#     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
+#     move.query_pose_nav(start_pose)
+#     # nlp stuff insert here
+#     image_switch_publisher.pub_now(ImageEnum.HANDOVER.value)
+#     text_to_speech_publisher.pub_now("The guest wants: apple, milk", talk)
+#
+# if step <= 4:
+#     text_to_speech_publisher.pub_now("Driving", talk)
+#     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
+#     move.query_pose_nav(robot_pose_to_human)
+#     # nlp stuff insert here
+#     image_switch_publisher.pub_now(ImageEnum.HANDOVER.value)
+#     text_to_speech_publisher.pub_now("Take out your order", talk)
+#
+# if step <= 5:
+#     text_to_speech_publisher.pub_now("Driving", talk)
+#     image_switch_publisher.pub_now(ImageEnum.DRIVINGBACK.value)
+#     move.query_pose_nav(start_pose)
+#     demo(step=1)
+
+
+demo(1)
