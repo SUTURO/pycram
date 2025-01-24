7230f06628351db1ba14de88b98f2182bce6c52b
MoAswad <aswad@uni-bremen.de>
2025-01-24 15:21:00 +0100

    [clean the table] updated complete plan

--- demos/pycram_clean_the_table_demo/utils/misc.py
+++ demos/pycram_clean_the_table_demo/utils/misc.py
@@ -1,5 +1,11 @@
+from typing_extensions import Optional
 from pycram.designators.action_designator import *
+from pycram.failures import PerceptionObjectNotFound
 from pycram.worlds.bullet_world import BulletWorld
+
+wished_sorted_obj_list_clean_the_table = ["Metalplate", "Metalbowl", "Metalmug", "Fork", "Spoon"]
+
+wished_sorted_obj_list_serve_breakfast = ["Metalbowl", "Cerealbox", "Milkpack", "Spoon"]
 
 
 def get_objects(obj_dict: dict):
@@ -14,11 +20,11 @@ def get_objects(obj_dict: dict):
     return objects_list
 
 
-def sort_objects(robot: BulletWorld.robot, obj_dict: dict, wished_sorted_obj_list: list):
+def sort_objects_euclidian(robot: BulletWorld.robot, found_objects_list: list, wished_sorted_obj_list: list):
     """
     Transforms the given object dictionary to a distance sorted list.
     The Metalplate, if seen, is arranged as the last object in the list.
-    :param obj_dict: tupel of State and dictionary of founded objects in the FOV
+    :param found_objects_list: list of found objects in the FOV
     :param wished_sorted_obj_list: list of object types we like to keep
     :param robot: the robot
     :return: distance sorted list of seen and wished to keep objects
@@ -28,21 +34,21 @@ def sort_objects(robot: BulletWorld.robot, obj_dict: dict, wished_sorted_obj_lis
     containsPlate = False
     metalplate = None
 
-    if len(obj_dict) == 0:
+    if len(found_objects_list) == 0:
         return sorted_objects
 
     # cut of the given State and keep the dictionary
     robot_pose = robot.get_pose()
     # calculate euclidian distance for all found object in a dictionary
     # for dictionary in remaining:
-    for value in obj_dict.values():
+    for value in found_objects_list:
         distance = math.sqrt(pow((value.pose.position.x - robot_pose.pose.position.x), 2) +
                              pow((value.pose.position.y - robot_pose.pose.position.y), 2) +
                              pow((value.pose.position.z - robot_pose.pose.position.z), 2))
 
         # fill dict with objects and their distance.
         # Add seen objects only once, if seen multiple times
-        if value.obj_type not in obj_dict:
+        if value.obj_type not in found_objects_list:
             object_dict[value.obj_type] = (value, distance)
 
     # sort all objects that where in the obj_dict
@@ -70,3 +76,22 @@ def sort_objects(robot: BulletWorld.robot, obj_dict: dict, wished_sorted_obj_lis
     print(test_list)
 
     return sorted_objects
+
+
+def try_detect(pose: Pose, technique: Optional[str] = None):
+    """
+    lets the robot looks on a pose and perceive objects or free spaces
+    :param pose: the pose that the robot looks to
+    :param technique: if location should be detected or not
+    :return: tupel of State and dictionary of found objects in the FOV
+    """
+    LookAtAction(targets=[pose]).resolve().perform()
+    TalkingMotion("Perceiving").perform()
+    try:
+        if technique == "location":
+            object_desig = DetectAction(technique='location', state='popcorn_table').resolve().perform()
+        else:
+            object_desig = DetectAction(technique='all').resolve().perform()
+    except PerceptionObjectNotFound:
+        object_desig = {}
+    return object_desig
