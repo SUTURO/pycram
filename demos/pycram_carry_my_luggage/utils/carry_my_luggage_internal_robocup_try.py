1e64a8c1eb3b4c121467bec2202e983203f8cd06
JuliSlz <juschulz@uni-bremen.de>
2025-02-18 09:59:13 +0100

    [CML] latest version unsed in internal robocup

--- demos/pycram_carry_my_luggage/utils/carry_my_luggage_internal_robocup_try.py
+++ demos/pycram_carry_my_luggage/utils/carry_my_luggage_internal_robocup_try.py
@@ -1,4 +1,7 @@
+import time
+
 from geometry_msgs.msg import PoseStamped
+from giskardpy.data_types.exceptions import ExecutionException
 
 import pycram.external_interfaces.giskard as giskardpy
 from demos.pycram_carry_my_luggage.utils.cml_helper import *
@@ -16,27 +19,31 @@ human = Human()
 start_pose = Pose([1, 1, 0])
 first_timer_pose = None
 second_timer_pose = None
-timeout1 = 10
+start_time = time.time()
+timeout1 = 15
 timeout2 = 25
 # Timer to check for no message
 
 
-
-
 def demo(step: int):
-    with real_robot:
+    global start_time
+    with (real_robot):
         img.pub_now(ImageEnum.HI.value)
 
         if step <= 1:
             rospy.sleep(1)
             print("start demo")
+
+            # store pose to drive back to
             start_pose = robot.get_pose()
             print("###########################")
             print(start_pose)
             print("###########################")
-            # ParkArmsAction([Arms.LEFT]).resolve().perform()
-            # TalkingMotion("beep boop").perform()
-            MoveJointsMotion(["head_tilt_joint"], [0.0]).perform()
+
+            # move robot in starting position
+            ParkArmsAction([Arms.LEFT]).resolve().perform()
+            MoveJointsMotion(["head_tilt_joint"], [0.2]).perform()
+            MoveJointsMotion(["head_pan_joint"], [0.0]).perform()
             MoveJointsMotion(["wrist_flex_joint"], [-1.6]).perform()
             MoveGripperMotion(GripperState.OPEN, Arms.LEFT).perform()
 
@@ -49,57 +56,69 @@ def demo(step: int):
             TalkingMotion("Push down my Hand, when we arrived.").perform()
 
             try:
+                # start timer /store time when following part starts
                 start_time = time.time()
+
+                # perceive and follow human
                 plan = Code(lambda: giskardpy.cml(False)) >> Monitor(monitor_func)
                 plan.perform()
+
             except SensorMonitoringCondition:
                 TalkingMotion("We have arrived.").perform()
                 img.pub_now(ImageEnum.PUSHBUTTONS.value)
                 rospy.sleep(1)
                 MoveJointsMotion(["wrist_flex_joint"], [-1.6]).perform()
                 TalkingMotion("I am not able to pick up the bag. Please hand it in my gripper").perform()
-                rospy.sleep(2)
+                rospy.sleep(4)
 
-                TalkingMotion("please place the bag in my gripper and push down my gripper").perform()
+                TalkingMotion("please put the bag in my gripper and push down my gripper").perform()
                 # TODO: Timer einbauen? falls gripper nicht gedrückt wird
 
                 img.pub_now(ImageEnum.PUSHBUTTONS.value)
                 try:
-                    plan = Code(lambda: rospy.sleep(1)) * 99999999 >> Monitor(monitor_func(start_time))
+                    plan = Code(lambda: rospy.sleep(1)) * 99999999 >> Monitor(monitor_func_no_timer)
                     plan.perform()
                 except SensorMonitoringCondition:
                     TalkingMotion("Closing my Gripper.").perform()
                     img.pub_now(ImageEnum.HI.value)
                     MoveGripperMotion(GripperState.CLOSE, Arms.LEFT).perform()
+                    if step <= 3:
+                        # drive back starting with last recorded pose
+                        drive_back_move_base(start_pose, first_timer_pose, second_timer_pose)
 
-                if step <= 3:
-                    print(start_pose)
-                    drive_back_move_base(start_pose, first_timer_pose, second_timer_pose)
 
-def monitor_func(start_time):
+def monitor_func():
     """
     monitors force torque sensor of robot and throws
     Condition if a significant force is detected (e.g. the gripper is pushed down)
     """
+    global start_time
+    global first_timer_pose
+    global second_timer_pose
+    global timeout1
+    global timeout2
+    global stored_poses
     der = fts.get_last_value()
     # print(der.wrench.force.x)
     if abs(der.wrench.force.x) > 10.30:
         rospy.logwarn("sensor exception")
         return SensorMonitoringCondition
+    if not first_timer_pose:
+        if int(time.time() - start_time) >= timeout1:
+            first_timer_pose = robot.get_pose()
+            rospy.loginfo("stored first drive pose")
+            print("###########################")
+            print(first_timer_pose)
+            print("###########################")
 
-    if int(time.time() - start_time) >= timeout1:
-        first_timer_pose = robot.get_pose()
-        rospy.loginfo("stored first drive pose")
-        print("###########################")
-        print(first_timer_pose)
-        print("###########################")
-
-    if int(time.time() - start_time) >= timeout2:
-        second_timer_pose = robot.get_pose()
-        rospy.loginfo("stored second drive pose")
-        print("###########################")
-        print(second_timer_pose)
-        print("###########################")
+    if not second_timer_pose:
+        if int(time.time() - start_time) >= timeout2:
+            second_timer_pose = robot.get_pose()
+            stored_poses = True
+            rospy.loginfo("stored second drive pose")
+            print("###########################")
+            print(second_timer_pose)
+            print("###########################")
 
     return False
 
@@ -112,8 +131,11 @@ def drive_back_move_base(start_pose, first_pose, second_pose):
     rospy.loginfo("driving back")
     TalkingMotion("Driving Back.").perform()
     img.pub_now(ImageEnum.DRIVINGBACK.value)
+
+    # was track long enough for second pose to be stored
     if second_pose:
         get_orientation(second_pose, first_pose)
+        # drive to the closest known pose
         NavigateAction([second_pose]).resolve().perform()
     if first_pose:
         get_orientation(first_pose, start_pose)
@@ -158,18 +180,20 @@ def demo_start(human: Human):
     The robot will wait until its hand is pushed down and then scan the
     environment for a human
     """
+    global start_time
     try:
 
-        #TalkingMotion("Starting Carry my Luggage demo.").perform()
-        img.pub_now(ImageEnum.HI.value)  # hi im toya
-        #TalkingMotion("Push down my Hand, when you are Ready.").perform()
+        TalkingMotion("Starting Carry my Luggage demo.").perform()
+        img.pub_now(ImageEnum.HI.value)
+        TalkingMotion("Push down my Hand, when you are Ready.").perform()
         img.pub_now(ImageEnum.PUSHBUTTONS.value)
-        plan = Code(lambda: rospy.sleep(1)) * 999999 >> Monitor(monitor_func)
+        plan = Code(lambda: rospy.sleep(1)) * 999999 >> Monitor(monitor_func_no_timer)
         plan.perform()
+
     except SensorMonitoringCondition:
         img.pub_now(ImageEnum.HI.value)
 
-        #TalkingMotion("Looking for a human").perform()
+        TalkingMotion("Looking for a human").perform()
         DetectAction(technique='human').resolve().perform()
         img.pub_now(ImageEnum.SEARCH.value)
         goal_msg = QueryGoal()
@@ -189,8 +213,17 @@ def demo_start(human: Human):
         return
 
 
+def monitor_func_no_timer():
+    """
+    monitors force torque sensor of robot and throws
+    Condition if a significant force is detected (e.g. the gripper is pushed down)
+    """
+    der = fts.get_last_value()
+    if abs(der.wrench.force.x) > 10.30:
+        rospy.logwarn("sensor exception, gripper pushed")
+        return SensorMonitoringCondition
 
-
+    return False
 
 
 demo(0)
